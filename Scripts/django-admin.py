ee502e5c76ce9808524a2bc9c2529976c0a8694d
Radicis <alloyddesign@gmail.com>
2014-12-17 13:37:34 +0000

    herko-asktag

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lotuste\Documents\GitHub\Asktag-heroku\asktag\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
