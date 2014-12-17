b02b87331a0385dff8a6c0653fd6e9ff14607297
Radicis <alloyddesign@gmail.com>
2014-12-17 14:18:11 +0000

    initial project commit

--- asktag/settings.py
+++ asktag/settings.py
@@ -24,6 +24,8 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 ALLOWED_HOSTS = []
 
 
@@ -82,6 +84,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 import dj_database_url
 import os
